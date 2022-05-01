e3d81974e5e8a229957a6dac8a35e84ebba59cad
mihai draghia <mihaidraghia25.md@gmail.com>
2022-05-02 00:23:20 +0200

    Cancellazione ToDoList

--- RentalProject.Test/Program.cs
+++ RentalProject.Test/Program.cs
@@ -10,45 +10,7 @@ namespace RentalProject.Test
     {
         static void Main(string[] args)
         {
-            //dettaglioVeicolo dataImmatricolazione calendar?...
 
-            // btnModificaCliente? e va su DettaglioCliente? query updateCliente...?
-            //e btn elimina ..query update ..set idTipoStato=12
-
-            //verifico query ricercaVeicolo
-            //verifico insertCliente
-            //verifico ricercaCliente
-
-
-
-
-            //campo ultimo ...disponibilita attuale veicolo?
-
-            //btn pulisci ricerca su cercaNoleggi
-            //rivere query ...isNoleggioAttivo...?
-
-            //gvNoleggi selectIndexCheìanged porta a GestioneNoleggiato... redirect passare idCliente...?
-
-
-            //noleggioModelView
-            //classe RicercaNoleggio in NoleggioManager , query RicercaNoleggio(ricercaNoleggio)...order by dataInizioNoleggio
-            //pagina ricercaNoleggio
-
-            //scegliere anche dataInserimento nella query ricerca e desc order by dataInserimento?
-            //togliere try parse query ricerca...
-            //cerca noleggio... pagina e query, order by desc dataInizioNoleggio (ordinato dal più recente?)
-            //singleton
-            //impaginazione
-            //veicoloControl con btn avanti indietro?
-
-            //3 parte home... Ricerca Noleggio...
-            //4 parte home.. Info Parco Auto? veicoli totali, veicoli attivi, veicoli disattivi, veicoli noleggiati...
-
-            //5 parte home inserisci cliente con btnInsertCliente
-            //6 parte home cerca cliente ..gvCliente selectIndexChanged...dat Cliente con btnModifica , elimina se non ha noleggi attivi..?
-
-
-            //cancello commenti test
         }
     }
 }
